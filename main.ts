0b3b3c4de73b67a558bf62d070e6d39ae77e3d51
wujunbao1963 <74924108+wujunbao1963@users.noreply.github.com>
2025-12-03 21:52:11 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -40,7 +40,7 @@ async function bootstrap() {
     maxAge: 3600,
   });
 
-  const port = config.get<number>('APP_PORT') ?? 3000;
+  const port = process.env.APP_PORT || process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
 
   console.log(`🚀 Application is running on: http://localhost:${port}`);
